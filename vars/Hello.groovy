62c0be6645033be450b175a40c17b46eed64c5ac
shabbier0473 <55012477+shabbier0473@users.noreply.github.com>
2020-07-03 18:39:38 +0530

    Update Hello.groovy

--- vars/Hello.groovy
+++ vars/Hello.groovy
@@ -1,10 +1,14 @@
-node{
-    stage('source'){
-        git url: 'https://github.com/shabbier0473/aa.git'
-        branch:  'master'
-    }
-    stage('compile'){
-        echo "hello"
-        
+def call(String command){
+    if  ( command == "hello" )
+    {
+      node{
+           stage('source'){
+           git url: 'https://github.com/shabbier0473/aa.git'
+           branch:  'master'
+          }
+          stage('compile'){
+                   echo "hello"
+           }
+      }
     }
 }
