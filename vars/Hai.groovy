7b07e389039dfab98d4d6bafee68febd12cfbcf5
shabbier0473 <55012477+shabbier0473@users.noreply.github.com>
2020-07-03 18:57:17 +0530

    Update Hai.groovy

--- vars/Hai.groovy
+++ vars/Hai.groovy
@@ -1,7 +1,8 @@
 def call(String command){
     if  ( command == "hai" )
     {
-      node{
+      node{ 
+          ws ("/var/lib/jenkins/workspace/${JOB_NAME}")
           stage('compile'){
                    sh "mvn install"
            }
