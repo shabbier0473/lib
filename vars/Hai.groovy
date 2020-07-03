7c55dadbae2d2e274c5dd57990e6d5b6640d30f8
shabbier0473 <55012477+shabbier0473@users.noreply.github.com>
2020-07-03 18:44:15 +0530

    Update Hai.groovy

--- vars/Hai.groovy
+++ vars/Hai.groovy
@@ -2,10 +2,6 @@ def call(String command){
     if  ( command == "hai" )
     {
       node{
-           stage('source'){
-           git url: 'https://github.com/shabbier0473/aa.git'
-           branch:  'master'
-          }
           stage('compile'){
                    echo "hai"
            }
