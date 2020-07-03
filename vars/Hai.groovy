953aeb15f05c60d1e3322f57939d600f2f8087b4
shabbier0473 <55012477+shabbier0473@users.noreply.github.com>
2020-07-03 18:42:24 +0530

    Create Hai.groovy

---
+++ vars/Hai.groovy
@@ -0,0 +1,14 @@
+def call(String command){
+    if  ( command == "hai" )
+    {
+      node{
+           stage('source'){
+           git url: 'https://github.com/shabbier0473/aa.git'
+           branch:  'master'
+          }
+          stage('compile'){
+                   echo "hello"
+           }
+      }
+    }
+}
